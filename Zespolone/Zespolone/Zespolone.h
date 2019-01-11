58442917c87bee5d72a6ce07019c63c746ab6c5e
Jakub Głatki <jakub.glatki@gmail.com>
2019-01-11 15:49:08 +0100

    zespolone

--- Zespolone/Zespolone/Zespolone.h
+++ Zespolone/Zespolone/Zespolone.h
@@ -1,1 +1,35 @@
 #pragma once
+#include "pch.h"
+#include <string.h>
+#include <stdio.h>
+#include <iostream>
+
+class zespolona {
+private:
+	double rzeczywista;
+	double urojona;
+
+public:
+	zespolona() {};
+	zespolona(double rzeczywista, double urojona);
+	double getRzeczywista();
+};
+
+
+class tabzes {
+private:
+	zespolona *tablica;
+	int rozmiar;
+
+public:
+	tabzes(int rozmiar);
+	zespolona& operator[](const int);
+	tabzes& operator= (const tabzes& tablica);
+	tabzes& operator= (tabzes&&tablica);
+	tabzes& operator+= (const zespolona &z);
+};
+
+
+class kolzes :public tabzes {
+
+};
