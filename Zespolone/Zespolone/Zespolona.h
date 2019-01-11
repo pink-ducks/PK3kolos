4f15df17e344bcedaf40fdae3fa0cdbdd8b1207b
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2019-01-11 14:43:59 +0100

    Zespolona 1) i 2)

---
+++ Zespolone/Zespolone/Zespolona.h
@@ -0,0 +1,32 @@
+#pragma once
+
+// 1A
+
+class zespolona
+{
+private:
+	double rzeczywista = 0;
+	double urojona = 0;
+public:
+	zespolona();
+	zespolona(double rzeczywista, double urojona);
+	// potrzebne metody i operatory
+
+};
+
+// 2A
+
+class tabzes
+{
+private:
+	zespolona * tablica;
+	int rozmiar;
+public:
+	tabzes(int rozmiar);
+	zespolona& operator[ ](const int rozmiar);  // 2B
+	tabzes& operator =(const tabzes& tablica); // 2C
+	tabzes& operator =(tabzes && tablica); // 2C
+	tabzes& operator+=(const zespolona &z); // 2D
+	
+
+};
